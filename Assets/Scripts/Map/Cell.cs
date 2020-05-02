8cef5dac645b7e5eb9b573f8ae104f5074efa6b1
Josh <jjoshpoland@gmail.com>
2020-05-02 10:45:29 -0400

    Grid created
    Grid component, cell component, some prefabs. Grid spawns in edit mode with a custom editor button. There is a map canvas that shows the coordinates of each cell.

---
+++ Assets/Scripts/Map/Cell.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TD.Map
+{
+    public class Cell : MonoBehaviour
+    {
+        [SerializeField]
+        Sprite Sprite;
+        Vector2Int coords;
+
+        public Vector2Int Coords { get => coords; set => coords = value; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
