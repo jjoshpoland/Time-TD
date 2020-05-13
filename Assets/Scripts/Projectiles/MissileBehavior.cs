9a199658e2add52221fe73fc56bdf57b137dca22
Josh <jjoshpoland@gmail.com>
2020-05-13 18:39:57 -0400

    missile turret logic added

---
+++ Assets/Scripts/Projectiles/MissileBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.iOS;
+using UnityEngine;
+
+namespace TD.Projectiles
+{
+    [CreateAssetMenu(fileName = "Missile", menuName = "TD/Projectiles/Missile", order = 2)]
+    public class MissileBehavior : ProjectileBehavior
+    {
+        public float speed;
+        public DamageType type;
+        public int damage;
+        public float explosionRadius;
+        public GameObject ExplosionEffect;
+
+        public override void Hit(Projectile projectile, GameObject target)
+        {
+
+            Collider[] hits = Physics.OverlapSphere(projectile.transform.position, explosionRadius, LayerMask.GetMask("Enemy"));
+
+            for (int i = 0; i < hits.Length; i++)
+            {
+                Health targetHealth = hits[i].gameObject.GetComponent<Health>();
+                if (targetHealth != null)
+                {
+                    targetHealth.TakeDamage(damage, type);
+                }
+            }
+
+            if (ExplosionEffect != null)
+            {
+                Instantiate(ExplosionEffect, projectile.transform);
+            }
+        }
+
+        public override void Initialize(Projectile projectile, Quaternion rotation)
+        {
+            
+        }
+
+        public override void Move(Projectile projectile, float scale, GameObject target)
+        {
+            if(target != null)
+            {
+                //Find the desired rotation by getting the difference between this position and the target position
+                Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - projectile.transform.position);
+
+                //Determine the amount of rotation by interpolating between the current rotation and the target rotation by a factor of time
+                Quaternion rotation = Quaternion.Slerp(projectile.transform.localRotation, targetRotation, 10f * Time.deltaTime); //turn the 20 into a parameter
+
+
+
+                //Set the local rotation to the interpolated rotation
+                projectile.transform.localRotation = rotation;
+            }
+            
+
+            projectile.transform.position += (projectile.transform.forward * (speed * scale)) * Time.deltaTime;
+        }
+    }
+
+}
